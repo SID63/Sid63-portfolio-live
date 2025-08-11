3e694448734effac9bad4d7b7d3359e490efc7d1
SID63 <iamsidarth@gmail.com>
2025-08-11 17:38:35 +0530

    analytics

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { Projects } from './components/Projects';
 import { Involvement } from './components/Involvement';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
